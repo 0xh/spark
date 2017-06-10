5a72b95305de5a206cbf509df1de03f4f384c2bb
root <root@homestead.localdomain>
2017-06-10 18:56:28 +0800

    addec metrouser config

---
+++ config/metrouser.php
@@ -0,0 +1,21 @@
+<?php 
+
+return [
+
+'mail_from' => 'johndoe@mail.com',
+'mail_from_name' => 'johndoe',
+'mail_invite_subject' => 'You are invited to join ',
+
+'app_name' => 'Metro App',
+'app_regis_url' => 'http://localhost/register',
+
+
+
+
+
+
+];
+
+
+
+ ?>
